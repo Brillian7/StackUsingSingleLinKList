a997bae28a5f4dbd157519eb0115e9f0b9c68c92
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-11-28 17:04:04 +0700

    add case 3

--- StackUsingSingleLinKList/Program.cs
+++ StackUsingSingleLinKList/Program.cs
@@ -86,6 +86,9 @@ namespace StackUsingSingleLinKList
                         }
                         s.pop();
                         break;
+                    case '3':
+                        s.display();
+                        break;
                     
 
                 }
